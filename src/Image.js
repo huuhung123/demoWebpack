85d140134096bedaae10c79dbf74237dfc2ce5cc
huuhung <dupbolun2012@gmail.com>
2020-05-08 14:43:31 +0700

    demoWebpack-first

---
+++ src/Image.js
@@ -0,0 +1,5 @@
+import './Image.css'
+
+const image = document.createElement('img');
+image.src = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.dammio.com%2F2019%2F08%2F16%2Fcss-selector-bo-chon-la-gi-va-cach-su-dung&psig=AOvVaw22whVaaI32pNmrzO7s9LwH&ust=1587207886177000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJDBlqyo7-gCFQAAAAAdAAAAABAD';
+document.body.appendChild(image)
---
+++ src/index.js
@@ -0,0 +1,8 @@
+// // var sum = require('./Math')
+import sum from './Math'
+import './Image'
+
+const total = sum(3, 5);
+console.log(total);
+document.write(total)
+
